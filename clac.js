31be0949a9b966c1877b9c4860c7d037f974758d
kumkum567 <yadavkumkum969@gmail.com>
2026-01-08 20:03:25 +0530

    Update clac.js

--- clac.js
+++ clac.js
@@ -1,19 +1,1 @@
-let element=document.getElementById("result")
-function appendvalue(value){
-    result.value+=value; 
-}
-function clearResult(){
-    result.value="";
-}
-function deletlast(){
-    result.value=result.value.slice(0,-1);
-}
-function calculate(){
-    try{
-        result.value=
-        eval(result.value);}
-        catch{
-            result.value="Error";
-        }
-    }
 
